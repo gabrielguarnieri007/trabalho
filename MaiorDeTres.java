936a34fedd8c0144acf8d4577f08f8e06ea1d59a
gabrielguarnieri007 <guarnieri99sousa@gmail.com>
2025-08-26 19:12:22 -0300

    Delete MaiorDeTres.java

--- MaiorDeTres.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-
-public class MaiorDeTres {
-    public static void main(String[] args) {
-       Scanner sc = new Scanner(System.in);
-     System.out.println("Digite o primeiro numero: ");
-    }
-}
