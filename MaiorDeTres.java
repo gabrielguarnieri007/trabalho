cdb2af5ca5d460ce09abdf0e0e5a8b5eee1e336b
gabrielguarnieri007 <guarnieri99sousa@gmail.com>
2025-08-26 19:11:34 -0300

    Add files via upload

---
+++ MaiorDeTres.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class MaiorDeTres {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+     System.out.println("Digite o primeiro numero: ");
+    }
+}
